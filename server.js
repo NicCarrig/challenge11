585015c61324c5a0e135db316c1f29f517400600
unknown <clemcarrig90@hotmail.com>
2022-06-16 03:28:57 -0700

    html routes

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const database = require('./db/db.json');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server now on port ${PORT}`);
+});
